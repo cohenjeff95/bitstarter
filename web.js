a38e32f52d4f31e33ceaface5b7f27c15b4a3095
cohenjeff95 <cohenjeff95@yahoo.com>
2013-07-14 00:44:24 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,19 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var fs = require('fs');
+var data = fs.readFileSync('index.html');
+  response.send(data.toString('utf-8'));
+
 });
-
-
-
-  var content;
-    fs.readFile('/index.html', function read(err, data) {
-        if (err) {
-            throw err;
-        }
-        content = data;
-    });
-    console.log(content);
 
 
 
